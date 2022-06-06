38336c36896bc57cc0f8daee12f0e4fb4f399008
Jesús Ángel <jesusgonzaleznovez@gmail.com>
2022-06-06 17:40:17 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <HelloWorld>
+    <template v-if="true" #buttons>
+      <h1>Hello World</h1>
+    </template>
+    <template v-else="false" #buttons>
+      <h1>Hello World</h1>
+    </template>
+  </HelloWorld>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
